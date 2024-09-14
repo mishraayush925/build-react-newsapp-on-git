676dc90ae5e5664d37917cb8b13a5c1509bee7f4
mishraayush925 <61752344+mishraayush925@users.noreply.github.com>
2024-09-14 19:41:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import {NewsItemsCard} from "./components/newsitems";
+import {newsObj} from "./components/newsSource";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Newsdetails} from "./components/newsdetails";
 
 function App() {
+  var _cardProp={
+    altText:"news article",
+    newsHeading: "Headline of your news",
+    readMore:"Read More",
+    newsSummary:"Some quick example text to build on the news and make up the bulk of the card's content."
+  };
+  var _newsExtractor = function(obj){
+        var returnNewsObj={
+          _title:obj.title,
+          _content:obj.content,
+          _description:obj.description,
+          _publishedAt:obj.publishedAt,
+          _imgUrl:obj.urlToImage,
+          _url:obj.url
+
+        };
+        return returnNewsObj;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <Header />
+      <div className="container">
+        <div className="centered-div">
+          <h2>Breaking News: Major Event Happening Now</h2>
+        </div>
+      </div>
+      <div className="items-container row">
+        
+      
+      {newsObj.articles.slice(0, 1).map(element => {
+       var currentItem=_newsExtractor(element);
+        return(<div key={element.url}  >
+          <NewsItemsCard cardProp={_cardProp} newsItem={currentItem} />
+                  </div>);
+      })}
+      </div>
+      
+      
+
+      <Footer />
+    </>
   );
 }
 
